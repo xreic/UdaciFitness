f444db7248ee9e1b689d91e47b049b278002d5ca
Eric Lau <ericlaucmo@gmail.com>
2020-05-28 19:42:42 -0700

    AddEntry component

---
+++ components/AddEntry.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { getMetricMetaInfo } from '../utils/helpers';
+
+class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  };
+
+  /**
+   * @param metric
+   * Receives a metric whose count needs to be incremented
+   */
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState((state) => {
+      const count = state[metric] + step;
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      };
+    });
+  };
+
+  /**
+   * @param metric
+   * Receives a metric whose count needs to be decremented
+   */
+  decrement = (metric) => {
+    this.setState((state) => {
+      const count = state[metric] - getMetricMetaInfo(metric).step;
+
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count
+      };
+    });
+  };
+
+  /**
+   * @param metric
+   * Receives a metric whose count will be adjusted to whatever the slider is set to
+   * @param value
+   * Value of the slider
+   */
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value
+    }));
+  };
+
+  render() {
+    return <View>{getMetricMetaInfo('bike').getIcon()}</View>;
+  }
+}
+
+export default AddEntry;
--- utils/helpers.js
+++ utils/helpers.js
@@ -8,82 +8,82 @@ import {
 } from '@expo/vector-icons';
 import { white } from './colors';
 
-export function getMetricMetaInfo(metric) {
-  const info = {
-    run: {
-      displayName: 'Run',
-      max: 50,
-      unit: 'miles',
-      step: 1,
-      type: 'steppers',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, { backgroundColor: red }]}>
-            <MaterialIcons name="directions-run" color={white} size={35} />
-          </View>
-        );
-      }
-    },
-    bike: {
-      displayName: 'Bike',
-      max: 100,
-      unit: 'miles',
-      step: 1,
-      type: 'steppers',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, { backgroundColor: orange }]}>
-            <MaterialCommunityIcons name="bike" color={white} size={32} />
-          </View>
-        );
-      }
-    },
-    swim: {
-      displayName: 'Swim',
-      max: 9900,
-      unit: 'meters',
-      step: 100,
-      type: 'steppers',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, { backgroundColor: blue }]}>
-            <MaterialCommunityIcons name="swim" color={white} size={35} />
-          </View>
-        );
-      }
-    },
-    sleep: {
-      displayName: 'Sleep',
-      max: 24,
-      unit: 'hours',
-      step: 1,
-      type: 'slider',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, { backgroundColor: lightPurp }]}>
-            <FontAwesome name="bed" color={white} size={30} />
-          </View>
-        );
-      }
-    },
-    eat: {
-      displayName: 'Eat',
-      max: 10,
-      unit: 'rating',
-      step: 1,
-      type: 'slider',
-      getIcon() {
-        return (
-          <View style={[styles.iconContainer, { backgroundColor: pink }]}>
-            <MaterialCommunityIcons name="food" color={white} size={35} />
-          </View>
-        );
-      }
-    }
-  };
+// export function getMetricMetaInfo(metric) {
+//   const info = {
+//     run: {
+//       displayName: 'Run',
+//       max: 50,
+//       unit: 'miles',
+//       step: 1,
+//       type: 'steppers',
+//       getIcon() {
+//         return (
+//           <View style={[styles.iconContainer, { backgroundColor: red }]}>
+//             <MaterialIcons name="directions-run" color={white} size={35} />
+//           </View>
+//         );
+//       }
+//     },
+//     bike: {
+//       displayName: 'Bike',
+//       max: 100,
+//       unit: 'miles',
+//       step: 1,
+//       type: 'steppers',
+//       getIcon() {
+//         return (
+//           <View style={[styles.iconContainer, { backgroundColor: orange }]}>
+//             <MaterialCommunityIcons name="bike" color={white} size={32} />
+//           </View>
+//         );
+//       }
+//     },
+//     swim: {
+//       displayName: 'Swim',
+//       max: 9900,
+//       unit: 'meters',
+//       step: 100,
+//       type: 'steppers',
+//       getIcon() {
+//         return (
+//           <View style={[styles.iconContainer, { backgroundColor: blue }]}>
+//             <MaterialCommunityIcons name="swim" color={white} size={35} />
+//           </View>
+//         );
+//       }
+//     },
+//     sleep: {
+//       displayName: 'Sleep',
+//       max: 24,
+//       unit: 'hours',
+//       step: 1,
+//       type: 'slider',
+//       getIcon() {
+//         return (
+//           <View style={[styles.iconContainer, { backgroundColor: lightPurp }]}>
+//             <FontAwesome name="bed" color={white} size={30} />
+//           </View>
+//         );
+//       }
+//     },
+//     eat: {
+//       displayName: 'Eat',
+//       max: 10,
+//       unit: 'rating',
+//       step: 1,
+//       type: 'slider',
+//       getIcon() {
+//         return (
+//           <View style={[styles.iconContainer, { backgroundColor: pink }]}>
+//             <MaterialCommunityIcons name="food" color={white} size={35} />
+//           </View>
+//         );
+//       }
+//     }
+//   };
 
-  return typeof metric === 'undefined' ? info : info[metric];
-}
+//   return typeof metric === 'undefined' ? info : info[metric];
+// }
 
 export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
@@ -203,5 +203,5 @@ export function getMetricMetaInfo(metric) {
     }
   };
 
-  return metric === undefined ? info : info[metric];
+  return typeof metric === undefined ? info : info[metric];
 }
