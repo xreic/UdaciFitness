de703e796548549819c0b89e747cba8a906c805e
Eric Lau <ericlaucmo@gmail.com>
2020-05-28 19:53:00 -0700

    init sliders and steppers component

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 import AddEntry from './components/AddEntry';
+import UdaciSlider from './components/UdaciSlider';
+import UdaciSteppers from './components/UdaciSteppers';
 
 export default function App() {
   return <AddEntry />;
---
+++ components/UdaciSteppers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UdaciSteppers = () => (
+  <View>
+    <Text>UdaciSteppers</Text>
+  </View>
+);
+
+export default UdaciSteppers;
