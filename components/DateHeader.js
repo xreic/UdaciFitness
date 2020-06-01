c92454588144ce2330c7eecb68660622b6d5519f
Eric Lau <ericlaucmo@gmail.com>
2020-06-01 16:02:43 -0700

    Additional styles

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { purple } from '../utils/colors';
 
-const DateHeader = ({ date }) => <Text>{date}</Text>;
+const DateHeader = ({ date }) => (
+  <Text style={{ color: purple, fontSize: 25 }}>{date}</Text>
+);
 
 export default DateHeader;
--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { View, Slider, Text } from 'react-native';
+import { View, Slider, Text, StyleSheet } from 'react-native';
+import { gray } from '../utils/colors';
 
 const UdaciSlider = ({ max, unit, step, value, onChange }) => (
-  <View>
+  <View style={styles.row}>
     <Slider
       step={step}
       value={value}
       maximumValue={max}
       minimumValue={0}
       onValueChange={onChange}
+      style={{ flex: 1 }}
     />
-    <View>
-      <Text>{value}</Text>
-      <Text>{unit}</Text>
+    <View style={styles.metricCounter}>
+      <Text style={{ fontSize: 24, textAlign: 'center' }}>{value}</Text>
+      <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
     </View>
   </View>
 );
 
 export default UdaciSlider;
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center'
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
